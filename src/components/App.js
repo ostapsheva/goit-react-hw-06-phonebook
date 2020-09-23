c89b5ccb1dd0c077ab7522a020c8b6d391393c0f
Ostap Shevchenko <ostapshev@gmail.com>
2020-09-23 03:54:37 +0300

    add task 06

--- src/components/App.js
+++ src/components/App.js
@@ -9,54 +9,6 @@ import './App.css';
 function App() {
   const contacts = useSelector(state => state.contacts.items);
 
-  // componentDidMount() {
-  //   const contacts = localStorage.getItem('contacts');
-  //   const parsedContacts = JSON.parse(contacts);
-
-  //   if (parsedContacts) {
-  //     this.setState({ contacts: parsedContacts });
-  //   }
-  // }
-
-  // componentDidUpdate(prevState) {
-  //   if (this.state.contacts !== prevState.contacts) {
-  //     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-  //   }
-  // }
-
-  // handleChange = event => {
-  //   this.setState({ name: event.target.value });
-  // };
-
-  // addContact = newContact => {
-  //   const { contacts } = this.state;
-  //   if (contacts.find(contact => contact.name === newContact.name)) {
-  //     this.setState({ alert: true });
-  //     setTimeout(() => this.setState({ alert: false }), 3000);
-  //   } else {
-  //     this.setState(prevState => {
-  //       return {
-  //         contacts: [...prevState.contacts, newContact],
-  //       };
-  //     });
-  //   }
-  // };
-
-  // onChangeFilter = filter => {
-  //   this.setState({ filter });
-  // };
-
-  // removeContact = contactId => {
-  //   this.setState(prevState => ({
-  //     contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-  //   }));
-  // };
-
-  // const { contacts, filter } = this.state;
-  // const filteredContacts = contacts.filter(contact =>
-  //   contact.name.toLowerCase().includes(filter),
-  // );
-
   return (
     <div className="main-container">
       <CSSTransition
